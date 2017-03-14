a79b2352a732724e7685f848064b0f816e7dc8e9
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-03-14 13:51:15 +0200

    Character controller, item pick ups

---
+++ Krampus/Assets/Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    public int distanceToItem = 1;
+
+    void Update()
+    {
+        Collect();
+    }
+
+    void Collect()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, distanceToItem))
+            {
+                if (hit.collider.gameObject.tag == "Item")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject.Find("PickUpText").GetComponent<Text>().text = "Press E to pick up item";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject.Find("PickUpText").GetComponent<Text>().text = "";
+        }
+    }
+}
