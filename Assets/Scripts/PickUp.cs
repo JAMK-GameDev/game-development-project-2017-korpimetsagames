4ce511b57e7cee187d1e39a4771fabe6fed0c8cc
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2017-03-14 15:32:05 +0200

    Fix gitignore

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    int distanceToItem = 2;
+
+    void Update()
+    {
+        Collect();
+    }
+
+    void Collect()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, distanceToItem))
+            {
+                if (hit.collider.gameObject.tag == "Item")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject.Find("PickUpText").GetComponent<Text>().text = "Press E to pick up item";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject.Find("PickUpText").GetComponent<Text>().text = "";
+        }
+    }
+}
