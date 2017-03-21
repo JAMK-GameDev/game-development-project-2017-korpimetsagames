8be736c540f77a7b7775b6ce40e6cc05c4a574ab
Jani Kähkönen <Jani Khknen>
2017-03-22 01:08:26 +0200

    Terrain, Pathfinding

---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour {
+    public Transform target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+
+        navMeshAgent.destination = target.position;
+    }
+}
