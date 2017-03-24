e1c8d2515d7b4b2dcab65ff30d6a582cefa5f71a
Jani Kähkönen <Jani Khknen>
2017-03-24 03:35:52 +0200

    Underwater, Pathfinding fix

---
+++ Assets/Scripts/Underwater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour {
+
+    public int waterLevel = 930;
+	
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < waterLevel - 1)
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogDensity = 0.2f;
+        }
+        else
+        {
+            RenderSettings.fog = false;
+            RenderSettings.fogDensity = 0.0f;
+        }
+    }
+}
