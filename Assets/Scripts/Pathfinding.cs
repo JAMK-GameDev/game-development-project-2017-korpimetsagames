90d534293ba9f8879ccc37329f73379535e55009
prome- <janne.mottola@hotmail.com>
2017-03-27 11:41:33 +0300

    Line of sight testing

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -5,16 +5,33 @@ using UnityEngine.AI;
 
 public class Pathfinding : MonoBehaviour {
     public Transform target;
-
+    private RaycastHit hit;
+    private Ray ray;
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start()   {    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+        Vector3 targetDir = target.transform.position - transform.position;
+        ray = new Ray(transform.position, targetDir);
+        navMeshAgent.ResetPath();
 
-        navMeshAgent.destination = target.position;
+        if (Physics.Raycast(ray, out hit))
+        {
+            print("Found an object: " + hit.transform.name);
+
+            if (hit.collider.tag.Equals("Player"))
+            {
+                navMeshAgent.destination = target.position;
+            }
+        }
+        else
+        {
+            print("I see nothing");
+        }
+
+
     }
 }
